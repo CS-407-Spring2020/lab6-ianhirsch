ed56597bf6a14a95cf2086e5365806ee92cb5d6a
IanHirsch <ihirsch@wisc.edu>
2020-03-06 17:04:19 -0600

    Milestone 2

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity {
 
     private final LatLng mDestinationLatLng = new LatLng(43.0752775, -89.4129);
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,49 @@ public class MainActivity extends FragmentActivity {
             googleMap.addMarker(new MarkerOptions()
             .position(mDestinationLatLng)
             .title("Destination"));
+            displayMyLocation();
         });
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation(){
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission== PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else{
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null){
+                            mMap.addPolyline(new PolylineOptions().add(
+                                    new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude()),
+                                    mDestinationLatLng));
+                        }
+                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                .findFragmentById(R.id.fragment_map);
+                        LatLng currentPosition = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                        mapFragment.getMapAsync(googleMap -> {
+                            mMap = googleMap;
+                            googleMap.addMarker(new MarkerOptions()
+                                    .position(currentPosition)
+                                    .title("Current Position"));
+                            displayMyLocation();
+                    });
+
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults){
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION){
+            if (grantResults.length>0
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                displayMyLocation();
+            }
+        }
     }
 }
